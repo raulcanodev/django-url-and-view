bc3e04920be65ccd82a7843ae2ed965bbb02cdff
Raul Marco <rawraul@Rauls-MacBook-Air.local>
2023-05-03 12:57:01 +0200

    montlhy challenges with dicctionary

--- challenges/views.py
+++ challenges/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    'january':'January connected',
+    'february': 'February connected',
+    'march': 'March connected',
+    'april': 'April connected',
+    'may': 'May connected',
+    'june': 'June connected',
+    'july': 'July connected',
+    'august': 'August connected',
+    'september': 'September connected',
+    'october' : 'October connected',
+    'november': 'November connected',
+    'december': 'December connected',
+}
 # Create your views here.
 
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
-# def january(request):
-#     return HttpResponse('January')
 
-# def february(request):
-#     return HttpResponse('February')
-
-# def march(request):
-#     return HttpResponse('March')
-
-def monthly_challenges(request, month):
-    text = None
-    if month == 'january':
-        text = 'January text'
-    elif month == 'february':
-        text = 'February text'
-    elif month == 'march':
-        text = 'March text'
-    else:
-        return HttpResponseNotFound('Month not supported')
-    return HttpResponse(text)
+def monthly_challenge(request, month):
+    challenge_text = monthly_challenges[month]
+    return HttpResponse(challenge_text)
+    
+    
