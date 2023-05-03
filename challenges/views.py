789c7689403cfc8288e3f4feeff8f0bc707f2468
Raul Marco <rawraul@Rauls-MacBook-Air.local>
2023-05-03 12:02:50 +0200

    Repaso dia 3 mayo bioclimatico

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+# def january(request):
+#     return HttpResponse('January')
+
+# def february(request):
+#     return HttpResponse('February')
+
+# def march(request):
+#     return HttpResponse('March')
+
+def monthly_challenges(request, month):
+    text = None
+    if month == 'january':
+        text = 'January text'
+    elif month == 'february':
+        text = 'February text'
+    elif month == 'march':
+        text = 'March text'
+    else:
+        return HttpResponseNotFound('Month not supported')
+    return HttpResponse(text)
